4e1d522afec49e66b1e01f372d1e80082ba87547
Krish Jain <krishjain2902@gmail.com>
2024-05-29 16:41:34 +0530

    First commit

--- public/frontend/assets/mail/contact_me.js
+++ public/frontend/assets/mail/contact_me.js
@@ -1,88 +1,72 @@
-$(function () {
-    $(
-        "#contactForm input,#contactForm textarea,#contactForm button"
-    ).jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function ($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function ($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(" ") >= 0) {
-                firstName = name.split(" ").slice(0, -1).join(" ");
-            }
-            $this = $("#sendMessageButton");
-            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-            $.ajax({
-                url: "/assets/mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
-                },
-                cache: false,
-                success: function () {
-                    // Success message
-                    $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
-                    );
-                    $("#success > .alert-success").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                error: function () {
-                    // Fail message
-                    $("#success").html("<div class='alert alert-danger'>");
-                    $("#success > .alert-danger")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-danger").append(
-                        $("<strong>").text(
-                            "Sorry " +
-                                firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
-                        )
-                    );
-                    $("#success > .alert-danger").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-                    }, 1000);
-                },
-            });
-        },
-        filter: function () {
-            return $(this).is(":visible");
-        },
-    });
+$("#contactForm").submit(function(event) {
+    event.preventDefault(); // prevent default form submission
+    // get values from FORM
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
+    var message = $("#message").val();
 
-    $('a[data-toggle="tab"]').click(function (e) {
-        e.preventDefault();
-        $(this).tab("show");
+    // Check for white space in name for Success/Fail message
+    var firstName = name.split(" ").slice(0, -1).join(" ");
+
+    $.ajax({
+        url: "http://localhost:8012/assets/mail/contact_me.php",
+        type: "POST",
+        data: {
+            name: name,
+            email: email,
+            phone: phone,
+            message: message,
+        },
+        cache: false,
+        success: function() {
+            // Success message
+            $("#success").html("<div class='alert alert-success'>");
+            $("#success > .alert-success")
+                .html(
+                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                )
+                .append("</button>");
+            $("#success > .alert-success").append(
+                "<strong>Your message has been sent.</strong>"
+            );
+            $("#success > .alert-success").append("</div>");
+
+            // Delay the redirection for a few seconds (e.g., 3 seconds)
+            setTimeout(function() {
+                window.location.href = "#";
+                // "http://localhost:8012/assets/mail/display_data.php";
+            }, 3000);
+        },
+
+        error: function() {
+            // Fail message
+            $("#success").html("<div class='alert alert-danger'>");
+            $("#success > .alert-danger")
+                .html(
+                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                )
+                .append("</button>");
+            $("#success > .alert-danger").append(
+                $("<strong>").text(
+                    "Sorry " +
+                    firstName +
+                    ", it seems that there was an issue submitting your message. Please try again later!"
+                )
+            );
+            $("#success > .alert-danger").append("</div>");
+            //clear all fields
+            $("#contactForm").trigger("reset");
+        },
     });
 });
 
+$('a[data-toggle="tab"]').click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+});
+
 /*When clicking on Full hide fail/success boxes */
-$("#name").focus(function () {
+$("#name").focus(function() {
     $("#success").html("");
 });
